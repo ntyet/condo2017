ac2053c15cfcd42a2ce25f1074aff0beb26f8f9f
Yet Nguyen <tienyettoan@gmail.com>
2018-05-22 15:56:28 -0500

    change ncores

--- example/Rjob.R
+++ example/Rjob.R
@@ -2,5 +2,5 @@
 library(parallel)
 cores = detectCores()
 print(cores)
-l = mclapply(1:(cores - 1), function(x) x^2, mc.cores = cores - 1)
+l = mclapply(1:(cores - 1), function(x) x^2, mc.cores = 6)
 print(unlist(l))
